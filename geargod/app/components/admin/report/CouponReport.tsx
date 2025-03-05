92505cec82b438760678a9266180f18b3902797a
Cherrine <66070031@kmitl.ac.th>
2025-03-05 17:54:08 +0700

    Update CouponReport.tsx

--- geargod/app/components/admin/report/CouponReport.tsx
+++ geargod/app/components/admin/report/CouponReport.tsx
@@ -1,7 +1,11 @@
+import {Progress} from "@heroui/react";
+
 export default function CouponReport(){
     return(
         <div className="w-96 h-20 bg-gradient-to-r from-gray-200 to-gray-400 shadow-lg rounded-lg p-4">
-            <p className="text-black">Coupon name</p>
+            <p className="text-black pb-4">Coupon name</p>
+            <Progress aria-label="Loading..." className="max-w-md" value={60} />
         </div>
     );
 }
+
